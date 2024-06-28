f91f1f7b878ce5538810a3abd567953635760cf1
TAMAGO55123 <147392915+TAMAGO55123@users.noreply.github.com>
2024-06-28 13:21:22 +0900

    test update

--- main.py
+++ main.py
@@ -30,7 +30,7 @@ with open(gradefile+'grade2.json', mode='w') as f:
     f.write(data2)
 
 with open(gradefile+'grade.js', mode='w') as f:
-    f.write('var gradedata=['+json.loads(data1)+','+json.loads(data2)+'];')
+    f.write('var gradedata=['+data1+','+data2+'];')
 
 os.system("git config user.name github-actions")
 os.system("git config user.email github-actions@github.com")
